716f814c4b1e19cdc8813003892d34122adf8999
J-Mara <jmaragakis20@stuy.edu>
2020-12-08 13:05:06 -0500

    first changes

--- ArraySequence.java
+++ ArraySequence.java
@@ -4,5 +4,33 @@ public class ArraySequence implements IntegerSequence{
   int []data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){  }
+  public ArraySequence(int[] other){
+    data = new int[other.length];
+    for(int i = 0; i < other.length; i++){
+      data[i] = other[i];
+    }
+    currentIndex = 0;
+  }
+
+  public void reset(){
+    currentIndex = 0;
+  }
+
+  public int length(){
+    return data.length;
+  }
+
+  public boolean hasNext(){
+    return (currentIndex <= length());
+  }
+
+  public int next(){
+    if(hasNext()){
+      currentIndex++;
+      return data[currentIndex-1];
+    }
+    else{
+      throw new NoSuchElementException();
+    }
+  }
 }
